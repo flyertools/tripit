297f261e973b7c40a1256cb2602e347e96ffb9aa
Andrew Glover <andrew@entitylab.com>
2011-03-20 00:56:01 +0100

    Empty test files

---
+++ test/unit/base_test.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/../helper'
+
+class BaseTest < Test::Unit::TestCase
+  # context "base" do
+  #     setup do
+  #       oauth = TripIt::OAuth.new('token', 'secret')
+  #       oauth.authorize_from_access('token', 'secret')
+  #       @tripit = TripIt::Base.new(oauth)
+  #     end
+  #     
+  #     should "require a client" do
+  #       assert_respond_to @tripit.client, :get
+  #       assert_respond_to @tripit.client, :post
+  #     end
+  #   end
+end
