9a6c2729565d69e96695310f779de53f32d360ad
Alex Kremer <alex@flextrip.com>
2011-03-21 01:17:16 +0100

    Bugfix in TpDateTime

--- lib/trip_it/classes/tp_date_time.rb
+++ lib/trip_it/classes/tp_date_time.rb
@@ -3,6 +3,7 @@ module TripIt
     attr_reader :date, :time
     
     def initialize(rubyDT)
+      return if rubyDT.nil?
       @date = rubyDT.strftime("%Y-%m-%d")
       @time = rubyDT.strftime("%H:%M:%S")
     end
